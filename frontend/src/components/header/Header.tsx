6fb607a4b14c96123b85c51ceb5add104ad1c333
Ciprian <cipri223223@gmail.com>
2022-12-01 17:13:09 +0200

    Header

---
+++ frontend/src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1 className="header_text">
+        Add your files containing the candidates emails
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
